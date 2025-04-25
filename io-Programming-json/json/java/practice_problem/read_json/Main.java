2b0323419c7074f18f0201c152f45ddcdf70c079
dar0408 <dsalgotra2021@gmail.com>
2025-04-25 15:43:03 +0530

    [Darpan] add : call JSON reader for student data in Main.java

---
+++ io-Programming-json/json/java/practice_problem/read_json/Main.java
@@ -0,0 +1,11 @@
+package practice_problem.read_json;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String filepath = "files/student.json";
+
+        ReadJson.readJson(filepath);
+
+    }
+}
